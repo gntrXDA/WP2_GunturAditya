3ff3e3f19dfcc0853de74cc28cd0df68e41c657d
gntrXDA <gntradty@gmail.com>
2021-10-06 20:56:01 +0700

    ci3fw

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Guntur Aditya <br>
+            Saya tinggal di Jakarta <br> 
+            Olahraga yg saya sukai adalah  
+            Kendo";
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    <h4>Selamat belajar Web Programming.. Jangan lupa titik koma</h4><br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
