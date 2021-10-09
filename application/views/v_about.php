8d09612da1b2b734c352fe34c60b5665954b6b21
gntrXDA <gntradty@gmail.com>
2021-10-09 21:59:34 +0700

    tugas bikin web buku

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Guntur</li>
+        <li>Nama Belakang : Aditya</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul>
+        <li>
+            Jalan Kebantenan 2
+        </li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type"square">
+        <li>Basket</li>
+        <li>Silat</li>
+    </ul>
+</section>
